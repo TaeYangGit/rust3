db3d23a36db0e087b834cc5cffbcbc5e38ebd21b
taeyanggi <taeyang.kim@gi-vita.io>
2022-08-04 19:04:08 +0900

    예제 3.2

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,15 @@ struct File {
     data: Vec<u8>,
 }
 
-impl File {
-    fn new(name: &str) -> File {
-        File {
-            name: String::from(name),
-            data: Vec::new(),
-        }
-    }
-}
-
 fn main() {
-    let f3 = File::new("f3.txt");
+    let f1 = File {
+        name: String::from("f1.txt"),
+        data: Vec::new(),
+    };
 
-    let f3_name = &f3.name;
-    let f3_length = f3.data.len();
+    let f1_name = &f1.name;
+    let f1_length = &f1.data.len();
 
-    println!("{:?}", f3);
-    println!("{} is {} bytes long", f3_name, f3_length);
+    println!("{:?}", f1);
+    println!("{} is {} bytes long", f1_name, f1_length);
 }
